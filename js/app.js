d70c1f3764db794735550ce64de2f8f4a67f63f9
praveenkumar <praveenkumar7501@gmail.com>
2020-06-03 00:43:24 +0530

    3-jun-20

--- js/app.js
+++ js/app.js
@@ -4,4 +4,28 @@ $(document).ready(function() {
         $('#main-wallpaper1').fadeToggle(2500);
         $('#main-wallpaper2').fadeToggle(2500);
     },5000);
+
+    
+    function testimonialLooping() {
+        var $datalist = $('#unique-journey-content .timeline-column');
+        $.each($datalist, function(index){
+            var self = this;
+            setTimeout(function () {
+                $(self).find('h6').addClass('active-journey-content');
+                $(self).find('hr').addClass('journey-ruler');
+                if(index > 0) {
+                    $(self).prev().find('h6').removeClass('active-journey-content');
+                    $(self).prev().find('hr').removeClass('journey-ruler');
+                }
+                if(index == $datalist.length-1) {
+                    setTimeout(function () {
+                        $(self).find('h6').removeClass('active-journey-content');
+                        $(self).find('hr').removeClass('journey-ruler');
+                        testimonialLooping();
+                    }, 3000);
+                }
+            }, index*3000);
+        });
+    }
+    testimonialLooping();
 });
