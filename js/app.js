d50a876e2d348e8121f4a58d55d8755381c43ec8
praveenkumar <praveenkumar7501@gmail.com>
2020-07-24 22:31:37 +0530

    24-july-2020-2

--- js/app.js
+++ js/app.js
@@ -64,6 +64,21 @@ $(document).ready(function() {
           });
         } 
     });
+
+    $("#scroll-t2s").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+  
+        var hash = this.hash;
+
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 500, function(){
+     
+          window.location.hash = hash;
+        });
+      } 
+  });
     
 });
 
